34aa7d9987e07aae909a5d274c2ccb31c31d31e2
jjmalz <50532220+jjmalz@users.noreply.github.com>
2019-10-09 17:31:15 -0400

    Improved documentation and order of function.

--- script.js
+++ script.js
@@ -1,69 +1,86 @@
+// The canvas
 var canvas = document.getElementById("maze");
+
+// Context variables
 var ctx = canvas.getContext("2d");
-var canvasText = document.getElementById("text");
 var context = canvasText.getContext("2d");
+
+// Text variables
+var canvasText = document.getElementById("text");
 var winText = document.getElementById("win");
+
+// Size of maze variables
 var rows = 15;
 var cols = rows;
 var total = rows * cols;
+
+// Node variables
 var nodes;
+var curNode;
+var visitedNodes;
+var neighbor;
+
+// Dimensions of each node in the maze
 var width = canvas.width / cols;
 var height = canvas.height / rows;
+
+// Variables to keep track of rows and columns
 var colStart;
 var rowStart;
-var radius = Math.min(width / 4, height / 4);
 var oldCol;
-var moves;
+var oldRow;
+var rowCur;
+var rowCur;
+
+// Clear path through the maze to show where the player can move
 var clearPath = [];
 
-function drawNodes() {
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 1;
-    // draw rows
-    for (let i = 0; i <= rows; i++) {
-        ctx.beginPath();
-        ctx.moveTo(0, i * height);
-        ctx.lineTo(canvas.width, i * height);
-        ctx.stroke();
-    }
-    // draw columns
-    for (let j = 0; j <= cols; j++) {
-        ctx.beginPath();
-        ctx.moveTo(j * width, 0);
-        ctx.lineTo(j * width, canvas.height);
-        ctx.stroke();
-    }
-}
+// Number of nodes visited by depth first search algorithm 
+var visited;
 
-function createClearPath() {
-    for (let i = 0; i < rows; i++) {
-        clearPath[i] = [];
-        for (let j = 0; j < cols; j++) {
-            clearPath[i][j] = [];
-        }
-    }
-}
+// Path currently being traversed by depth first search algorithm 
+var path;
 
-function generateClearPath() {
-    clearPath[oldRow][oldCol].push([rowCur, colCur]);
-    clearPath[rowCur][colCur].push([oldRow, oldCol]);
-}
+// Number of moves the player has made
+var moves;
 
-function checkClearPath() {
-    for (let i = 0; i < clearPath[oldRow][oldCol].length; i++) {
-        if (clearPath[oldRow][oldCol][i][0] == rowCur && clearPath[oldRow][oldCol][i][1] == colCur) {
-            return true;
-        }
-    }
-    return false;
+// Radius for ball representing the player
+var radius = Math.min(width / 4, height / 4);
+
+// Start the game!
+play();
+
+function play() {
+    // The main function for the maze. Setup the maze and allow the player to play it
+    
+    window.addEventListener("keydown", keyActionDown);
+    nodes = [];
+    generateNodes()
+    pickStart();
+    path = [[rowStart, colStart]];
+    neighbor = null;
+    rowCur = rowStart;
+    colCur = colStart;
+    visited = 1;
+    visitedNodes = [[rowStart, colStart]];
+    oldRow;
+    curNode;
+    generatePath();
+    rowCur = 0;
+    colCur = 0;
+    moves = 0;
+    interval = setInterval(draw, 10);
 }
 
 function generateNodes() {
+    // Generate the nodes represented as a nested list
+
     for (let i = 0; i < rows; i++) {
         nodes[i] = [];
         for (let j = 0; j < cols; j++) {
+            // each i, jth entry of nodes represent the neighboring cells
             // neighboring cells format: left, up, right, down
-            // ordered row, col, wall exists 
+            // nodes in following order: row, col, wall exists 
             nodes[i][j] = [[i, j - 1, 1, "left"], [i - 1, j, 1, "up"], [i, j + 1, 1, "right"], [i + 1, j, 1, "down"]];
             // remove cells that don't exist
             if (nodes[i][j][3][0] >= rows) {
@@ -83,8 +100,30 @@ function generateNodes() {
     drawNodes();
 }
 
+function drawNodes() {
+    // Draw the nodes in the maze
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
+    // draw rows
+    for (let i = 0; i <= rows; i++) {
+        ctx.beginPath();
+        ctx.moveTo(0, i * height);
+        ctx.lineTo(canvas.width, i * height);
+        ctx.stroke();
+    }
+    // draw columns
+    for (let j = 0; j <= cols; j++) {
+        ctx.beginPath();
+        ctx.moveTo(j * width, 0);
+        ctx.lineTo(j * width, canvas.height);
+        ctx.stroke();
+    }
+}
+
 function pickStart() {
-    // Math.random picks number between 0(inclusive) and 1(exclusive)
+    // Randomly pick a starting column and row somewhere on the outer section of the maze
+
     rowStart = Math.floor(Math.random() * rows);  
     if (rowStart == 0 || rowStart == rows - 1) {
         colStart = Math.ceil(Math.random() * (cols - 1));
@@ -94,42 +133,9 @@ function pickStart() {
     }    
 }
 
-function checkVisited(node) {
-    if (visitedNodes.length == 0) {
-        return false;
-    }
-    for (var i = 0; i < visitedNodes.length; i++) {
-        if (node[0] == visitedNodes[i][0] && node[1] == visitedNodes[i][1]) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function removeWall(row, col, direction) {
-    ctx.beginPath();
-    if (direction == "left") {
-        ctx.moveTo(col * width, row * height + 0.6);
-        ctx.lineTo(col * width, (row + 1) * height - 0.6);
-    }
-    else if (direction == "up") {
-        ctx.moveTo(col * width + 0.6, row * height);
-        ctx.lineTo((col + 1) * width - 0.6, row * height);
-    }
-    else if (direction == "right") {
-        ctx.moveTo((col + 1) * width, row * height + 0.6);
-        ctx.lineTo((col + 1) * width, (row + 1) * height - 0.6);
-    }
-    else {
-        ctx.moveTo(col * width + 0.6, (row + 1) * height);
-        ctx.lineTo((col + 1) * width - 0.6, (row + 1) * height);
-    }
-    ctx.strokeStyle = "white";
-    ctx.lineWidth = 3;
-    ctx.stroke();
-}
-
 function generatePath() {
+    // Generate the path through the maze that the player can move through
+
     createClearPath();
     while (visited < total) {
         while (neighbor == null && nodes[rowCur][colCur].length != 0) {
@@ -162,7 +168,66 @@ function generatePath() {
     }
 }
 
+function createClearPath() {
+    // Create an empty list to represent the clear path for the player
+
+    for (let i = 0; i < rows; i++) {
+        clearPath[i] = [];
+        for (let j = 0; j < cols; j++) {
+            clearPath[i][j] = [];
+        }
+    }
+}
+
+function checkVisited(node) {
+    // Return true if the node has been visited and false otherwise
+
+    if (visitedNodes.length == 0) {
+        return false;
+    }
+    for (var i = 0; i < visitedNodes.length; i++) {
+        if (node[0] == visitedNodes[i][0] && node[1] == visitedNodes[i][1]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function removeWall(row, col, direction) {
+    // Remove the wall at the given row and col in the specified direction by making the line white
+
+    ctx.beginPath();
+    if (direction == "left") {
+        ctx.moveTo(col * width, row * height + 0.6);
+        ctx.lineTo(col * width, (row + 1) * height - 0.6);
+    }
+    else if (direction == "up") {
+        ctx.moveTo(col * width + 0.6, row * height);
+        ctx.lineTo((col + 1) * width - 0.6, row * height);
+    }
+    else if (direction == "right") {
+        ctx.moveTo((col + 1) * width, row * height + 0.6);
+        ctx.lineTo((col + 1) * width, (row + 1) * height - 0.6);
+    }
+    else {
+        ctx.moveTo(col * width + 0.6, (row + 1) * height);
+        ctx.lineTo((col + 1) * width - 0.6, (row + 1) * height);
+    }
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 3;
+    ctx.stroke();
+}
+
+function generateClearPath() {
+    // Generate the clear path for the player
+
+    clearPath[oldRow][oldCol].push([rowCur, colCur]);
+    clearPath[rowCur][colCur].push([oldRow, oldCol]);
+}
+
 function draw() {
+    // Draw the maze with the player position and end 
+
     ctx.clearRect(oldCol * width + 1, oldRow * height + 1, width - 2, height - 2);
     drawEnd();
     drawMoves()
@@ -179,6 +244,8 @@ function draw() {
 }
 
 function drawEnd() {
+    // Draw the end
+
     let endX = canvas.width - width;
     let endY = canvas.height - height;
     ctx.beginPath();
@@ -188,7 +255,18 @@ function drawEnd() {
     ctx.closePath();
 }
 
+function drawMoves() {
+    // Draw the number of moves below the maze that the player has made
+
+    context.clearRect(0, 0, canvasText.width, canvasText.height);
+    context.font = "16px Arial";
+    context.fillStyle = "blue";
+    context.fillText("Moves: " + moves, 0, 16);
+  }
+
 function keyActionDown(key) {
+    // Change the row or column depending on what key was pressed
+
     oldCol = colCur;
     oldRow = rowCur;
     // left
@@ -208,6 +286,7 @@ function keyActionDown(key) {
     else if (key.keyCode == 40 || key.keyCode == 83) {
         rowCur += 1;
     }
+    // make sure the move was ina  valid spot
     if (colCur > cols - 1 || colCur < 0) {
         colCur = oldCol;
         moves -= 1;
@@ -224,44 +303,20 @@ function keyActionDown(key) {
     draw();
 }
 
-function drawMoves() {
-    context.clearRect(0, 0, canvasText.width, canvasText.height);
-    context.font = "16px Arial";
-    context.fillStyle = "blue";
-    context.fillText("Moves: " + moves, 0, 16);
-  }
+function checkClearPath() {
+    // 
 
-var path;
-var neighbor;
-var rowCur;
-var colCur;
-var visited;
-var visitedNodes;
-var oldRow;
-var curNode;
-play();
-
-function play() {
-    window.addEventListener("keydown", keyActionDown);
-    nodes = [];
-    generateNodes()
-    pickStart();
-    path = [[rowStart, colStart]];
-    neighbor = null;
-    rowCur = rowStart;
-    colCur = colStart;
-    visited = 1;
-    visitedNodes = [[rowStart, colStart]];
-    oldRow;
-    curNode;
-    generatePath();
-    rowCur = 0;
-    colCur = 0;
-    moves = 0;
-    interval = setInterval(draw, 10);
+    for (let i = 0; i < clearPath[oldRow][oldCol].length; i++) {
+        if (clearPath[oldRow][oldCol][i][0] == rowCur && clearPath[oldRow][oldCol][i][1] == colCur) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function win() {
+    // Display the win message and allow the player to press the R key to reset the maze
+
     clearInterval(interval);
     window.removeEventListener("keydown", keyActionDown); 
     winText.style.display = "block";
@@ -270,6 +325,8 @@ function win() {
 }
 
 function resetGame(key) {
+    // Reset the game with a new maze if the R key was pressed
+
     if (key.keyCode == 82) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         window.removeEventListener("keydown", resetGame); 
